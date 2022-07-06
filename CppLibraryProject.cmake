717cbb383eaa30a438583299ba75fc437a0c25e4
Aymeric Pellé <aympelle@gmail.com>
2022-07-06 15:16:13 +0200

    v0.7:#26: #WIP  Merge install_honly_cpp_library() and install_cpp_library().

--- CppLibraryProject.cmake
+++ CppLibraryProject.cmake
@@ -142,6 +142,7 @@ function(add_cpp_honly_library library_name)
 endfunction()
 
 # params:
+#  HEADER_ONLY
 #  SHARED
 #  STATIC
 #  [NAMESPACE]
@@ -152,50 +153,34 @@ function(install_cpp_library)
   include(GNUInstallDirs)
   # Args:
   set(options "")
-  set(params "SHARED;STATIC;EXPORT;DESTINATION;NAMESPACE")
+  set(params "HEADER_ONLY;SHARED;STATIC;EXPORT;DESTINATION;NAMESPACE")
   set(lists "")
   # Parse args:
   cmake_parse_arguments(PARSE_ARGV 0 "ARG" "${options}" "${params}" "${lists}")
   # Check/Set args:
-  fatal_if_none_is_def("You did not choose which target(s) to build (build_shared, build_static)." ARG_SHARED ARG_STATIC)
-  set_ifndef(ARG_STATIC "")
-  set_ifndef(ARG_SHARED "")
+  fatal_if_none_is_def("You did not choose which target(s) to install (SHARED, STATIC, HEADER_ONLY)." ARG_SHARED ARG_STATIC ARG_HEADER_ONLY)
   set_ifndef(ARG_EXPORT "${PROJECT_NAME}-targets")
   set_ifndef(ARG_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
   set_iftest(namespace_opt IF ARG_NAMESPACE THEN NAMESPACE ${ARG_NAMESPACE})
   # Install targets:
-  install(TARGETS ${ARG_SHARED} ${ARG_STATIC} EXPORT ${ARG_EXPORT}
-          FILE_SET HEADERS DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-          )
+  if(DEFINED ARG_SHARED OR DEFINED ARG_STATIC)
+    fatal_ifdef("The library to install is either a SHARED/STATIC library or a HEADER_ONLY library, not both." ARG_HEADER_ONLY)
+    set_ifndef(ARG_STATIC "")
+    set_ifndef(ARG_SHARED "")
+    install(TARGETS ${ARG_SHARED} ${ARG_STATIC} EXPORT ${ARG_EXPORT}
+            FILE_SET HEADERS DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+            )
+  else()
+    install(TARGETS ${ARG_HEADER_ONLY} EXPORT ${ARG_EXPORT})
+    target_include_directories(${ARG_HEADER_ONLY} INTERFACE $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+    get_target_property(HBDS ${ARG_HEADER_ONLY} CMTK_HLIB_HEADERS_BASE_DIRS)
+    foreach(include_dir ${HBDS})
+      install(DIRECTORY ${include_dir}/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+    endforeach()
+  endif()
   # Install export:
   install(EXPORT ${ARG_EXPORT} DESTINATION ${ARG_DESTINATION} ${namespace_opt})
   export(EXPORT ${ARG_EXPORT} FILE ${CMAKE_CURRENT_BINARY_DIR}/${ARG_EXPORT}.cmake ${namespace_opt})
-endfunction()
-
-function(install_honly_cpp_library)
-  include(GNUInstallDirs)
-  # Args:
-  set(options "")
-  set(params "HEADER_ONLY;EXPORT;DESTINATION;NAMESPACE")
-  set(lists "")
-  # Parse args:
-  cmake_parse_arguments(PARSE_ARGV 0 "ARG" "${options}" "${params}" "${lists}")
-  # Check and set args:
-  fatal_ifndef("You must provide a HEADER_ONLY target." ARG_HEADER_ONLY)
-  set_ifndef(ARG_EXPORT "${PROJECT_NAME}-targets")
-  set_ifndef(ARG_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
-  set_iftest(namespace_opt IF ARG_NAMESPACE THEN NAMESPACE ${ARG_NAMESPACE})
-  # Install targets:
-  install(TARGETS ${ARG_HEADER_ONLY} EXPORT ${ARG_EXPORT})
-  # Install includes:
-  target_include_directories(${ARG_HEADER_ONLY} INTERFACE $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
-  get_target_property(HBDS ${ARG_HEADER_ONLY} CMTK_HLIB_HEADERS_BASE_DIRS)
-  foreach(include_dir ${HBDS})
-    install(DIRECTORY ${include_dir}/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-  endforeach()
-  # Install export:
-  install(EXPORT ${ARG_EXPORT} DESTINATION ${ARG_DESTINATION}  ${namespace_opt})
-  export(EXPORT ${ARG_EXPORT} FILE ${CMAKE_CURRENT_BINARY_DIR}/${ARG_EXPORT}.cmake ${NAMESPACE_OPT})
 endfunction()
 
 function(install_package package_name)
