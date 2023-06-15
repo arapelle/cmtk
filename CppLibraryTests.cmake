8378ea955661dc043f8d2e742a0b0db5b0ead772
Aymeric Pellé <aympelle@gmail.com>
2023-06-15 14:58:40 +0200

    v0.8:#29: Move find_or_fetch_GTest() to its own source file.

--- CppLibraryTests.cmake
+++ CppLibraryTests.cmake
@@ -1,29 +1,6 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/Utility.cmake)
-
-macro(find_or_fetch_GTest)
-    if(NOT TARGET GTest::gtest_main)
-        cmake_parse_arguments("ARG" "QUIET" "TAG" "" ${ARGN})
-        include(GoogleTest)
-        set(fp_args)
-        if(ARG_QUIET)
-            list(APPEND fp_args "QUIET")
-        endif()
-        find_package(GTest ${fp_args})
-        if(NOT GTest_FOUND)
-            if(NOT ARG_TAG)
-                set(ARG_TAG "v1.13.0")
-            endif()
-            include(FetchContent)
-            FetchContent_Declare(
-              googletest
-              URL https://github.com/google/googletest/archive/refs/tags/${ARG_TAG}.zip
-            )
-            set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-            FetchContent_MakeAvailable(googletest)
-        endif()
-    endif()
-endmacro()
+include(${CMAKE_CURRENT_LIST_DIR}/FindOrFetchGTest.cmake)
 
 function(add_cpp_library_test test_name gtest_target)
     # Args:
