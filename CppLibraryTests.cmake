768177b7d8bfcfb5803ad25e7888c4633979518e
Aymeric Pellé <aympelle@gmail.com>
2023-06-15 16:51:00 +0200

    v0.8: Improve copy_dll target name.

--- CppLibraryTests.cmake
+++ CppLibraryTests.cmake
@@ -10,12 +10,13 @@ function(add_cpp_library_test test_name gtest_target)
     # Parse args:
     cmake_parse_arguments(PARSE_ARGV 2 "ARG" "${options}" "${params}" "${lists}")
     # Check args:
+    string(SHA1 bdir_hash "${CMAKE_CURRENT_BINARY_DIR}")
     fatal_if_none_is_def("Provide SHARED, STATIC or HEADER_ONLY library target!" 
                          ARG_STATIC ARG_SHARED ARG_HEADER_ONLY)
     if(ARG_SHARED AND TARGET ${ARG_SHARED})
         set(library_name ${ARG_SHARED})
-        if(WIN32 AND NOT TARGET copy_dll_${library_name})
-            add_custom_target(copy_dll_${library_name} ALL
+        if(WIN32 AND NOT TARGET copy_dll_${bdir_hash})
+            add_custom_target(copy_dll_${bdir_hash} ALL
                 ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${library_name}> ${CMAKE_CURRENT_BINARY_DIR})
         endif()
     elseif(ARG_STATIC AND TARGET ${ARG_STATIC})
@@ -29,8 +30,8 @@ function(add_cpp_library_test test_name gtest_target)
     #
     add_executable(${test_name} ${ARG_SOURCES} ${ARG_HEADERS})
     target_link_libraries(${test_name} PRIVATE ${library_name} ${ARG_DEPENDENCIES} ${gtest_target})
-    if(WIN32 AND TARGET copy_dll_${library_name})
-        add_dependencies(${test_name} copy_dll_${library_name})
+    if(WIN32 AND TARGET copy_dll_${bdir_hash})
+        add_dependencies(${test_name} copy_dll_${bdir_hash})
     endif()
     gtest_discover_tests(${test_name} TEST_PREFIX ${test_prog}::)
 endfunction()
@@ -43,12 +44,13 @@ function(add_cpp_library_basic_tests gtest_target)
     # Parse args:
     cmake_parse_arguments(PARSE_ARGV 1 "ARG" "${options}" "${params}" "${lists}")
     # Check args:
+    string(SHA1 bdir_hash "${CMAKE_CURRENT_BINARY_DIR}")
     fatal_if_none_is_def("Provide SHARED, STATIC or HEADER_ONLY library target!" 
                          ARG_STATIC ARG_SHARED ARG_HEADER_ONLY)
     if(ARG_SHARED AND TARGET ${ARG_SHARED})
         set(library_name ${ARG_SHARED})
-        if(WIN32 AND NOT TARGET copy_dll_${library_name})
-            add_custom_target(copy_dll_${library_name} ALL
+        if(WIN32 AND NOT TARGET copy_dll_${bdir_hash})
+            add_custom_target(copy_dll_${bdir_hash} ALL
                 ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${library_name}> ${CMAKE_CURRENT_BINARY_DIR})
         endif()
     elseif(ARG_STATIC AND TARGET ${ARG_STATIC})
@@ -63,8 +65,8 @@ function(add_cpp_library_basic_tests gtest_target)
         get_filename_component(test_prog ${filename} NAME_WE)
         add_executable(${test_prog} ${filename})
         target_link_libraries(${test_prog} PRIVATE ${library_name} ${ARG_DEPENDENCIES} ${gtest_target})
-        if(WIN32 AND TARGET copy_dll_${library_name})
-            add_dependencies(${test_prog} copy_dll_${library_name})
+        if(WIN32 AND TARGET copy_dll_${bdir_hash})
+            add_dependencies(${test_prog} copy_dll_${bdir_hash})
         endif()
         gtest_discover_tests(${test_prog} TEST_PREFIX ${test_prog}::)
     endforeach()
