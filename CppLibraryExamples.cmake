58da281278112c75d2221295a2776161cc226e19
Aymeric Pellé <aympelle@gmail.com>
2023-11-21 11:28:30 +0100

    v0.12.0:#54: Replace add_cpp_library_examples() by add_cpp_library_basic_examples() and add_cpp_library_example().

--- CppLibraryExamples.cmake
+++ CppLibraryExamples.cmake
@@ -1,7 +1,37 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/Utility.cmake)
 
-function(add_cpp_library_examples)
+function(add_cpp_library_example example_name)
+    # Args:
+    set(options "")
+    set(params "STATIC;SHARED;HEADER_ONLY")
+    set(lists "HEADERS;SOURCES;DEPENDENCIES")
+    # Parse args:
+    cmake_parse_arguments(PARSE_ARGV 0 "ARG" "${options}" "${params}" "${lists}")
+    # Check args:
+    string(SHA1 bdir_hash "${CMAKE_CURRENT_BINARY_DIR}")
+    fatal_if_none_is_def("Provide SHARED, STATIC or HEADER_ONLY library target!" 
+                         ARG_STATIC ARG_SHARED ARG_HEADER_ONLY)
+    if(ARG_SHARED AND TARGET ${ARG_SHARED})
+        set(library_name ${ARG_SHARED})
+    elseif(ARG_STATIC AND TARGET ${ARG_STATIC})
+        set(library_name ${ARG_STATIC})
+    elseif(ARG_HEADER_ONLY AND TARGET ${ARG_HEADER_ONLY})
+        set(library_name ${ARG_HEADER_ONLY})
+    endif()
+    fatal_ifndef("You must provide a list of example source files." ARG_SOURCES)
+    #
+    add_executable(${example_name} ${ARG_SOURCES} ${ARG_HEADERS})
+    target_link_libraries(${example_name} PRIVATE ${library_name} ${ARG_DEPENDENCIES})
+    if(WIN32)
+        set_target_properties(${example_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${example_name}")
+        add_custom_command(TARGET ${example_name} POST_BUILD 
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_name}> $<TARGET_FILE_DIR:${example_name}>
+            COMMAND_EXPAND_LISTS)
+    endif()
+endfunction()
+
+function(add_cpp_library_basic_examples)
     # Args:
     set(options "")
     set(params "STATIC;SHARED;HEADER_ONLY")
@@ -14,10 +44,6 @@ function(add_cpp_library_examples)
                          ARG_STATIC ARG_SHARED ARG_HEADER_ONLY)
     if(ARG_SHARED AND TARGET ${ARG_SHARED})
         set(library_name ${ARG_SHARED})
-        if(WIN32 AND NOT TARGET copy_dll_${bdir_hash})
-            add_custom_target(copy_dll_${bdir_hash} ALL
-                ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${library_name}> ${CMAKE_CURRENT_BINARY_DIR})
-        endif()
     elseif(ARG_STATIC AND TARGET ${ARG_STATIC})
         set(library_name ${ARG_STATIC})
     elseif(ARG_HEADER_ONLY AND TARGET ${ARG_HEADER_ONLY})
@@ -29,8 +55,11 @@ function(add_cpp_library_examples)
         get_filename_component(example_prog ${filename} NAME_WE)
         add_executable(${example_prog} ${filename})
         target_link_libraries(${example_prog} PRIVATE ${library_name} ${ARG_DEPENDENCIES})
-        if(WIN32 AND TARGET copy_dll_${bdir_hash})
-            add_dependencies(${example_prog} copy_dll_${bdir_hash})
+        if(WIN32)
+            set_target_properties(${example_prog} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${example_prog}")
+            add_custom_command(TARGET ${example_prog} POST_BUILD 
+                              COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_prog}> $<TARGET_FILE_DIR:${example_prog}>
+                              COMMAND_EXPAND_LISTS)
         endif()
     endforeach()
 endfunction()
--- doc/CppLibraryExamples.md
+++ doc/CppLibraryExamples.md
@@ -5,7 +5,18 @@
 `cmtk/CppExecutableTests`
 
 ## Functions
-### Function `add_cpp_library_examples(...)`
+### Function `add_cpp_library_example(target_name ...)`
+
+&ensp;&ensp;&ensp;&ensp;Create an identified C++ example target for a library.
+
+- [STATIC *static_target*]: 	The static library target.
+- [SHARED *shared_target*]: 	The shared library target.
+- [HEADER_ONLY *header_only_target*]: 	The header-only library target.
+- [HEADERS *header_list*]: 	The list of C++ headers to compile the example target.
+- SOURCES *source_list*: 	The list of C++ sources to compile the example target.
+- [LIBRARIES *dependency_list*]: 	The list of dependency library targets.
+
+### Function `add_cpp_library_basic_examples(...)`
 
 &ensp;&ensp;&ensp;&ensp;Create C++ example targets for a library.
 
