eaf5428940867ef94ccdb7b728501a142f17c691
Aymeric Pellé <aympelle@gmail.com>
2024-01-05 15:23:47 +0100

    v0.13.0#60: An empty file is created to prevent copy_if_different to fail when there is no runtime libs to copy.

--- CppLibraryExamples.cmake
+++ CppLibraryExamples.cmake
@@ -26,7 +26,8 @@ function(add_cpp_library_example example_name)
     if(WIN32)
         set_target_properties(${example_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${example_name}")
         add_custom_command(TARGET ${example_name} POST_BUILD 
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_name}> $<TARGET_FILE_DIR:${example_name}>
+            COMMAND ${CMAKE_COMMAND} -E touch $<TARGET_FILE_DIR:${example_name}>/.dummy.txt
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_name}> $<TARGET_FILE_DIR:${example_name}>/.dummy.txt $<TARGET_FILE_DIR:${example_name}>
             COMMAND_EXPAND_LISTS)
     endif()
 endfunction()
@@ -58,7 +59,8 @@ function(add_cpp_library_basic_examples)
         if(WIN32)
             set_target_properties(${example_prog} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${example_prog}")
             add_custom_command(TARGET ${example_prog} POST_BUILD 
-                              COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_prog}> $<TARGET_FILE_DIR:${example_prog}>
+                              COMMAND ${CMAKE_COMMAND} -E touch $<TARGET_FILE_DIR:${example_prog}>/.dummy.txt
+                              COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${example_prog}> $<TARGET_FILE_DIR:${example_prog}>/.dummy.txt $<TARGET_FILE_DIR:${example_prog}>
                               COMMAND_EXPAND_LISTS)
         endif()
     endforeach()
